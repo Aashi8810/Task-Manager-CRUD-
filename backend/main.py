d8d523b1ddb9971501aceb6f6ce34d7b75f3acc9
Aashi8810 <aashinshanaweerakoon@gmail.com>
2026-03-04 00:30:25 +0530

    crud and main updated

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,51 @@
-from fastapi import FastAPI
-from database import engine
-import models
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+import models, schemas, crud
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+# Dependency Injection
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/tasks/", response_model=schemas.TaskResponse)
+def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
+    return crud.create_task(db, task)
+
+
+@app.get("/tasks/", response_model=list[schemas.TaskResponse])
+def read_tasks(db: Session = Depends(get_db)):
+    return crud.get_tasks(db)
+
+
+@app.get("/tasks/{task_id}", response_model=schemas.TaskResponse)
+def read_task(task_id: int, db: Session = Depends(get_db)):
+    task = crud.get_task(db, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
+
+
+@app.put("/tasks/{task_id}", response_model=schemas.TaskResponse)
+def update_task(task_id: int, task: schemas.TaskUpdate, db: Session = Depends(get_db)):
+    updated_task = crud.update_task(db, task_id, task)
+    if not updated_task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return updated_task
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    deleted_task = crud.delete_task(db, task_id)
+    if not deleted_task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {"message": "Task deleted successfully"}
